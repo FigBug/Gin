69caae9c826bb4a29dbd84f6cb8e31cce34c7d29
Roland Rabien <roland@rabien.com>
2019-03-12 10:39:57 -0700

    Updated Demo project for juce 5.4.3

--- examples/Demo/JuceLibraryCode/JuceHeader.h
+++ examples/Demo/JuceLibraryCode/JuceHeader.h
@@ -41,6 +41,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Demo";
+    const char* const  companyName    = "";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
