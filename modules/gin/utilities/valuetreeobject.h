1a890dbcb94acc9eb44740211001e82f101e4b84
Roland Rabien <figbug@gmail.com>
2019-07-07 08:45:51 -0700

    Adding some ValueTree listeners

--- modules/gin/utilities/valuetreeobject.h
+++ modules/gin/utilities/valuetreeobject.h
@@ -7,6 +7,7 @@
 
 #pragma once
 
+//==============================================================================
 class ValueTreeAllListener : public ValueTree::Listener
 {
 public:
@@ -20,6 +21,170 @@ public:
     void valueTreeRedirected (ValueTree&) override {}
 };
 
+//==============================================================================
+class LambdaValueTreeListener : public ValueTree::Listener
+{
+public:
+    LambdaValueTreeListener (ValueTree& v_)
+        : v (v_)
+    {
+        v.addListener (this);
+    }
+    
+    ~LambdaValueTreeListener()
+    {
+        v.removeListener (this);
+    }
+
+    std::function<void (ValueTree&, const Identifier&)> onValueTreePropertyChanged;
+    std::function<void (ValueTree&, ValueTree&)> onValueTreeChildAdded;
+    std::function<void (ValueTree&, ValueTree&, int)> onValueTreeChildRemoved;
+    std::function<void (ValueTree&, int, int)> onValueTreeChildOrderChanged;
+    std::function<void (ValueTree&)> onValueTreeParentChanged;
+    std::function<void (ValueTree&)> onValueTreeRedirected;
+    
+private:
+    void valueTreePropertyChanged (ValueTree&v , const Identifier& i) override
+    {
+        if (onValueTreePropertyChanged)
+            onValueTreePropertyChanged (v, i);
+    }
+    
+    void valueTreeChildAdded (ValueTree& p, ValueTree& c) override
+    {
+        if (onValueTreeChildAdded)
+            onValueTreeChildAdded (p, c);
+    }
+    
+    void valueTreeChildRemoved (ValueTree& p, ValueTree& c, int n) override
+    {
+        if (onValueTreeChildRemoved)
+            onValueTreeChildRemoved (p, c, n);
+        
+    }
+    
+    void valueTreeChildOrderChanged (ValueTree& v, int a, int b) override
+    {
+        if (onValueTreeChildOrderChanged)
+            onValueTreeChildOrderChanged (v, a, b);
+    }
+    
+    void valueTreeParentChanged (ValueTree& v) override
+    {
+        if (onValueTreeParentChanged)
+            onValueTreeParentChanged (v);
+    }
+    
+    void valueTreeRedirected (ValueTree& v) override
+    {
+        if (onValueTreeRedirected)
+            onValueTreeRedirected (v);
+    }
+    
+    ValueTree& v;
+};
+
+//==============================================================================
+class AsyncLambdaValueTreeListener : public ValueTree::Listener
+{
+public:
+    AsyncLambdaValueTreeListener (ValueTree& v_)
+        : v (v_)
+    {
+        v.addListener (this);
+    }
+    
+    ~AsyncLambdaValueTreeListener()
+    {
+        v.removeListener (this);
+        
+        masterReference.clear();
+    }
+    
+    std::function<void (const ValueTree&, const Identifier&)> onValueTreePropertyChanged;
+    std::function<void (const ValueTree&, const ValueTree&)> onValueTreeChildAdded;
+    std::function<void (const ValueTree&, const ValueTree&, int)> onValueTreeChildRemoved;
+    std::function<void (const ValueTree&, int, int)> onValueTreeChildOrderChanged;
+    std::function<void (const ValueTree&)> onValueTreeParentChanged;
+    std::function<void (const ValueTree&)> onValueTreeRedirected;
+    
+private:
+    void valueTreePropertyChanged (ValueTree& v, const Identifier& i) override
+    {
+        ValueTree vc = v;
+        Identifier ic = i;
+        WeakReference<AsyncLambdaValueTreeListener> weakThis (this);
+        MessageManager::getInstance()->callAsync ([this, weakThis, vc, ic]
+                                                  {
+                                                      if (weakThis != nullptr && onValueTreePropertyChanged)
+                                                          onValueTreePropertyChanged (vc, ic);
+                                                  });
+    }
+    
+    void valueTreeChildAdded (ValueTree& p, ValueTree& c) override
+    {
+        ValueTree pc = p;
+        ValueTree cc = c;
+        WeakReference<AsyncLambdaValueTreeListener> weakThis (this);
+        MessageManager::getInstance()->callAsync ([this, weakThis, pc, cc]
+                                                  {
+                                                      if (weakThis != nullptr && onValueTreeChildAdded)
+                                                          onValueTreeChildAdded (pc, cc);
+                                                  });
+    }
+    
+    void valueTreeChildRemoved (ValueTree& p, ValueTree& c, int n) override
+    {
+        ValueTree pc = p;
+        ValueTree cc = c;
+        WeakReference<AsyncLambdaValueTreeListener> weakThis (this);
+        MessageManager::getInstance()->callAsync ([this, weakThis, pc, cc, n]
+                                                  {
+                                                      if (weakThis != nullptr && onValueTreeChildRemoved)
+                                                          onValueTreeChildRemoved (pc, cc, n);
+                                                  });
+    }
+    
+    void valueTreeChildOrderChanged (ValueTree& v, int a, int b) override
+    {
+        ValueTree vc = v;
+        WeakReference<AsyncLambdaValueTreeListener> weakThis (this);
+        MessageManager::getInstance()->callAsync ([this, weakThis, vc, a, b]
+                                                  {
+                                                      if (weakThis != nullptr && onValueTreeChildOrderChanged)
+                                                          onValueTreeChildOrderChanged (vc, a, b);
+                                                  });
+    }
+    
+    void valueTreeParentChanged (ValueTree& v) override
+    {
+        ValueTree vc = v;
+        WeakReference<AsyncLambdaValueTreeListener> weakThis (this);
+        MessageManager::getInstance()->callAsync ([this, weakThis, vc]
+                                                  {
+                                                      if (weakThis != nullptr && onValueTreeParentChanged)
+                                                          onValueTreeParentChanged (vc);
+                                                  });
+    }
+    
+    void valueTreeRedirected (ValueTree& v) override
+    {
+        ValueTree vc = v;
+        WeakReference<AsyncLambdaValueTreeListener> weakThis (this);
+        MessageManager::getInstance()->callAsync ([this, weakThis, vc]
+                                                  {
+                                                      if (weakThis != nullptr && onValueTreeRedirected)
+                                                          onValueTreeRedirected (vc);
+                                                  });
+    }
+    
+    ValueTree& v;
+    
+    WeakReference<AsyncLambdaValueTreeListener>::Master masterReference;
+    friend class WeakReference<AsyncLambdaValueTreeListener>;
+};
+
+//==============================================================================
 /* Mirrors a ValueTree is Objects */
 class ValueTreeObject : private ValueTree::Listener
 {
