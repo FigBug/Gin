7a463c86d214234d48b9cdfae7471d3c6e033739
Roland Rabien <roland@rabien.com>
2019-11-19 12:34:26 -0800

    Set juce module path

--- ci/win/build.bat
+++ ci/win/build.bat
@@ -10,7 +10,7 @@ cd "%ROOT%\juce\extras\Projucer\Builds\VisualStudio2017"
 "%MSBUILD_EXE%" Projucer.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-.\x64\Release\App\Projucer.exe --set-global-search-path osx defaultJuceModulePath "%ROOT%\juce\modules" 
+.\x64\Release\App\Projucer.exe --set-global-search-path windows defaultJuceModulePath "%ROOT%\juce\modules" 
 .\x64\Release\App\Projucer.exe --resave "%ROOT%\examples\Demo\Demo.jucer"
 
 cd "%ROOT%\examples\Demo\Builds\VisualStudio2017"
