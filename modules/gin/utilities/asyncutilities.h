20a395da4a93edf4c3c758d5642b8e04701469db
Roland Rabien <roland@rabien.com>
2019-12-02 17:16:09 -0800

    Added LambdaAsyncUpdater

--- modules/gin/utilities/asyncutilities.h
+++ modules/gin/utilities/asyncutilities.h
@@ -10,6 +10,8 @@
 class LambdaTimer : public Timer
 {
 public:
+	LambdaTimer (std::function<void()> func = nullptr) : onTimer (func) {}
+
     std::function<void()> onTimer;
 
 private:
@@ -18,6 +20,21 @@ private:
         if (onTimer)
             onTimer();
     }
+};
+
+class LambdaAsyncUpdater : public AsyncUpdater
+{
+public:
+	LambdaAsyncUpdater (std::function<void()> func) : onAsyncUpdate (func) {}
+
+	std::function<void()> onAsyncUpdate;
+
+private:
+	void handleAsyncUpdate () override
+	{
+		if (onAsyncUpdate)
+			onAsyncUpdate();
+	}
 };
 
 void callOnMainThreadBlocking ( std::function<void ()> func );
