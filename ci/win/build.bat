93b3e39aeb992ed4cfb7989403148bbffd489a59
Roland Rabien <roland@rabien.com>
2020-02-23 14:35:16 -0800

    Use VS2019

--- ci/win/build.bat
+++ ci/win/build.bat
@@ -14,15 +14,15 @@ set ROOT=%cd%
 
 git clone https://github.com/WeAreROLI/JUCE.git --branch develop --single-branch juce
 
-cd "%ROOT%\juce\extras\Projucer\Builds\VisualStudio2017"
-"%MSBUILD_EXE%" Projucer.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64 
+cd "%ROOT%\juce\extras\Projucer\Builds\VisualStudio2019"
+"%MSBUILD_EXE%" Projucer.sln /p:VisualStudioVersion=16.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 .\x64\Release\App\Projucer.exe --set-global-search-path windows defaultJuceModulePath "%ROOT%\juce\modules" 
 .\x64\Release\App\Projucer.exe --resave "%ROOT%\examples\Demo\Demo.jucer"
 
-cd "%ROOT%\examples\Demo\Builds\VisualStudio2017"
-"%MSBUILD_EXE%" Demo.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64 
+cd "%ROOT%\examples\Demo\Builds\VisualStudio2019"
+"%MSBUILD_EXE%" Demo.sln /p:VisualStudioVersion=16.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 mkdir "%ROOT%\ci\win\bin"
