6cbc23c615edc33bbddaefc1bf41f552c9f751f5
Michael Hartmann <michael.hartmann@refx.com>
2020-05-04 16:47:12 -0700

    Simplification.

--- modules/gin/images/imageutilities.cpp
+++ modules/gin/images/imageutilities.cpp
@@ -5,23 +5,17 @@
 
  ==============================================================================*/
 
-juce::Image rasterizeSVG (juce::String svgText, int w, int h)
+juce::Image rasterizeSVG ( juce::String svgText, int w, int h )
 {
-    Image img (Image::ARGB, w, h, true);
-    Graphics g (img);
+	Image       img ( Image::ARGB, w, h, true );
+	Graphics    g ( img );
 
-    if (auto svg = XmlDocument::parse (svgText))
-    {
-        std::unique_ptr<Drawable> drawable;
+	if ( auto svg = XmlDocument::parse ( svgText ) )
+	{
+		const MessageManagerLock mmLock;
+		auto	drawable = Drawable::createFromSVG ( *svg );
+		drawable->drawWithin ( g, Rectangle<float> ( float ( w ), float ( h ) ), 0, 1.f );
+	}
 
-        {
-            const MessageManagerLock mmLock;
-            drawable = Drawable::createFromSVG (*svg);
-            drawable->setTransformToFit (Rectangle<float> (0.0f, 0.0f, float (w), float (h)), RectanglePlacement::centred);
-            drawable->draw (g, 1.f);
-        }
-
-    }
-
-    return img;
+	return img;
 }
