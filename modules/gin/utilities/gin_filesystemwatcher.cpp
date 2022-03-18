f0a938de592f13f806a90649856dc26f1c257b57
Roland Rabien <figbug@gmail.com>
2022-03-18 13:03:33 -0700

    FileSystemWatcher:   Ignore folder watched multiple times

--- modules/gin/utilities/gin_filesystemwatcher.cpp
+++ modules/gin/utilities/gin_filesystemwatcher.cpp
@@ -345,7 +345,8 @@ void FileSystemWatcher::addFolder (const juce::File& folder)
     // You can only listen to folders that exist
     jassert (folder.isDirectory());
 
-    watched.add (new Impl (*this, folder));
+    if ( ! getWatchedFolders().contains (folder))
+        watched.add (new Impl (*this, folder));
 }
 
 void FileSystemWatcher::removeFolder (const juce::File& folder)
