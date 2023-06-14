0e31c410927dd748495525485d7939564b943ade
Roland Rabien <figbug@gmail.com>
2023-06-14 15:45:38 -0700

    Link curl

--- ci/build.sh
+++ ci/build.sh
@@ -52,7 +52,7 @@ if [ "$(uname)" == "Darwin" ]; then
 # Build linux version
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   sudo apt-get update
-  sudo apt-get install clang git ninja-build ladspa-sdk freeglut3-dev g++ libasound2-dev libcurl4-openssl-dev libfreetype6-dev libjack-jackd2-dev libx11-dev libxcomposite-dev libxcursor-dev libxinerama-dev libxrandr-dev mesa-common-dev webkit2gtk-4.0 juce-tools xvfb  
+  sudo apt-get install clang git ninja-build ladspa-sdk libgtk-3-dev freeglut3-dev g++ libasound2-dev libcurl4-openssl-dev libfreetype6-dev libjack-jackd2-dev libx11-dev libxcomposite-dev libxcursor-dev libxinerama-dev libxrandr-dev mesa-common-dev webkit2gtk-4.0 juce-tools xvfb  
 
   cd "$ROOT"
   cmake --preset ninja-gcc
