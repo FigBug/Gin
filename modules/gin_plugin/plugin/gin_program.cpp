a3ebb2f521e4caa069579ae24d426ad833ed81c6
Roland Rabien <figbug@gmail.com>
2023-10-05 05:58:22 -0700

    Fix build

--- modules/gin_plugin/plugin/gin_program.cpp
+++ modules/gin_plugin/plugin/gin_program.cpp
@@ -19,7 +19,7 @@ void Program::loadProcessor (Processor& p)
 
     if (w != -1) p.state.setProperty ("width", w, nullptr);
     if (h != -1) p.state.setProperty ("height", h, nullptr);
-    if (sc > 0) p.state.setProperty ("editorScale", s, nullptr);
+    if (sc > 0) p.state.setProperty ("editorScale", sc, nullptr);
 
     for (const auto& s : states)
         if (auto pp = p.getParameter (s.uid))
