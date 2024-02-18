24c8d27c19289764342099cd38a8e81f064bc09a
Roland Rabien <figbug@gmail.com>
2024-02-17 16:38:51 -0800

    Remove ;

--- modules/gin/utilities/gin_diff.h
+++ modules/gin/utilities/gin_diff.h
@@ -4,4 +4,4 @@ namespace Diff
 {
     std::vector<uint8_t> bsDiff (const juce::String& s1, const juce::String& s2);
     juce::String bsApplyPatch (const juce::String& s, const std::vector<uint8_t>& patch);
-};
+}
