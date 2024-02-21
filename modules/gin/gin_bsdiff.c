5cc3b8cfcdabeaaaa9ad6c38133103edc0c84701
Roland Rabien <figbug@gmail.com>
2024-02-21 10:14:01 -0800

    Ignore warning

--- modules/gin/gin_bsdiff.c
+++ modules/gin/gin_bsdiff.c
@@ -1,2 +1,7 @@
+#if defined (__clang__)
+ #pragma clang diagnostic ignored "-Wimplicit-int-conversion"
+ #pragma clang diagnostic ignored "-Wsign-conversion"
+#endif
+
 #include "3rdparty/bsdiff/bsdiff.c"
 #include "3rdparty/bsdiff/bspatch.c"
