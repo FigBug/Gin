5cf9f653d3e0f5d35ba18bf5d95795eef5a35f80
Roland Rabien <figbug@gmail.com>
2024-03-09 13:40:35 -0800

    Change text

--- modules/gin_plugin/components/gin_msegcomponent.cpp
+++ modules/gin_plugin/components/gin_msegcomponent.cpp
@@ -335,7 +335,7 @@ void MSEGComponent::mouseDrag (const juce::MouseEvent& e)
             ts = juce::String::formatted ("%.2fs", p.time * 1.0f / rate->getUserValue());
         }
 
-        showBubbleMessage (r.expanded (5), ts + juce::String::formatted (": %.1f%%", p.value * 100));
+        showBubbleMessage (r.expanded (5), ts + juce::String::formatted (": %.1f%%", (p.value + 1.0f) / 2.0f * 100));
     }
     else if (draggingCurve >= 0)
     {
