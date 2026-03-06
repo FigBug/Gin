6e4a64f2595c388bfb0eb88d5a2cf1712910a020
Roland Rabien <figbug@gmail.com>
2026-03-06 08:40:24 -0800

    Increase thread timeout

--- modules/gin/utilities/gin_realtimeasyncupdater.cpp
+++ modules/gin/utilities/gin_realtimeasyncupdater.cpp
@@ -18,7 +18,7 @@ public:
     {
         signalThreadShouldExit ();
         event.signal();
-        stopThread (1000);
+        stopThread (5000);
     }
     
     void registerUpdater (RealtimeAsyncUpdater* ras)
